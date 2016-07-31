061bf1f8b82ca1b108be55ed11333abde41333f1
vagrant <jbaek.in@gmail.com>
2016-07-31 11:48:25 +0000

    * add downloading svntools.gradle scripts

--- gsvn.bat
+++ gsvn.bat
@@ -4,6 +4,14 @@ REM below codes are not tested on windows environment yet !!!
 
 IF NOT EXIST ".\gradle\wrapper\gradle-wrapper.jar" (
 	set DOWNURL=https://raw.githubusercontent.com/appbakers/gradlew.zip/master/gradlew.zip
+	echo "gradle-wrapper.jar not exists. downloading..."
 	@powershell -command "& { (New-Object Net.WebClient).DownloadFile(%DOWNURL%, './') }"
 )
+
+IF NOT EXIST ".\svntools.gradle" (
+	set DOWNURL=https://raw.githubusercontent.com/appbakers/svntools/master/svntools.gradle
+	echo "svntools.gradle not exists. downloading..."
+	@powershell -command "& { (New-Object Net.WebClient).DownloadFile(%DOWNURL%, './') }"
+)
+
 gradlew.bat -b .\svntools.gradle %*
