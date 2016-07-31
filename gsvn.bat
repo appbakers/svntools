e616a4c2859dcb4edaadcf81b9e1695de07a5c82
vagrant <jbaek.in@gmail.com>
2016-07-31 11:58:23 +0000

    * correct order. correct typo

--- gsvn.bat
+++ gsvn.bat
@@ -2,16 +2,16 @@ REM simple gradle subversion
 
 REM below codes are not tested on windows environment yet !!!
 
+IF NOT EXIST ".\svntools.gradle" (
+	set DOWNURL=https://raw.githubusercontent.com/appbakers/svntools/master/svntools.gradle
+	echo "svntools.gradle not exists. downloading..."
+	@powershell -command "& { (New-Object Net.WebClient).DownloadFile(%DOWNURL%, './') }"
+)
 IF NOT EXIST ".\gradle\wrapper\gradle-wrapper.jar" (
 	set DOWNURL=https://raw.githubusercontent.com/appbakers/gradlew.zip/master/gradlew.zip
 	echo "gradle-wrapper.jar not exists. downloading..."
 	@powershell -command "& { (New-Object Net.WebClient).DownloadFile(%DOWNURL%, './') }"
 )
 
-IF NOT EXIST ".\svntools.gradle" (
-	set DOWNURL=https://raw.githubusercontent.com/appbakers/svntools/master/svntools.gradle
-	echo "svntools.gradle not exists. downloading..."
-	@powershell -command "& { (New-Object Net.WebClient).DownloadFile(%DOWNURL%, './') }"
-)
 
-gradlew.bat -b .\svntools.gradle %*
+gradlew.bat --daemon -b .\svntools.gradle %*
